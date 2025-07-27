4e8b1018d6f401a36c8c01159ea800e4d6ae3146
Lahari <laharireddy.kotha@gmail.com>
2025-07-27 13:42:14 +0530

    Week-6 React

---
+++ Week-6/myapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Welcome to Home page of my student management portal</h1>
+      <h1>Welcome to About page of my student management portal</h1>
+      <h1>Welcome to Home page of my student management portal</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
